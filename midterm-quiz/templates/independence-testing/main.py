530a2d79f84138346b4d89dc3d3afda222fb82d2
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-03-11 15:25:07 -0600

    chi2 script

--- midterm-quiz/templates/independence-testing/main.py
+++ midterm-quiz/templates/independence-testing/main.py
@@ -1,1 +1,12 @@
 # Here Ivan xd
+from scipy.stats import chi2_contingency 
+  
+data = [[207, 282, 241], [234, 242, 232]] 
+stat, p, dof, expected = chi2_contingency(data) 
+  
+alpha = 0.05
+print("p value is " + str(p)) 
+if p <= alpha: 
+    print('Dependent(reject H0)') 
+else: 
+    print('Independent(H0 holds true)')
