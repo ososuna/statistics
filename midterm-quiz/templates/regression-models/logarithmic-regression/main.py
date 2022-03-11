c585168afb4b2b48f8f4bff2f8bfeb2b231f9f0c
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-03-10 23:22:35 -0600

    Logarithmic regression function

--- midterm-quiz/templates/regression-models/logarithmic-regression/main.py
+++ midterm-quiz/templates/regression-models/logarithmic-regression/main.py
@@ -2,14 +2,16 @@ import numpy as np
 from numpy.linalg import inv 
 import matplotlib.pyplot as plt
 
-def quadraticRegression(x, y):
-  qm = np.poly1d(np.polyfit(x, y, 2))
-  y_pred = qm(x)
+def logarithmicRegression(x, y):
+  fit = np.polyfit(np.log(x), y, 1)
+  y_pred = []
+  for i in x:
+    y_pred.append(fit[1] + fit[0] * np.log(i))
   plt.scatter(x, y)
   plt.plot(x, y_pred, c='blue', linewidth=2)
   plt.show()
 
-quadraticRegression(
+logarithmicRegression(
   [2.50, 3, 4, 5, 5.5, 6, 7],
   [12.5, 10, 7, 4.50, 4, 3, 3.5]
 )
