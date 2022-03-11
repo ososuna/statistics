6c30503c3fd70349ed8247473de9466064ff9962
Oswaldo Adrián Osuna <oswaldo.adrian@live.com.mx>
2022-03-11 00:09:28 -0600

    Added lr w least square

--- midterm-quiz/templates/regression-models/linear-regression/main.py
+++ midterm-quiz/templates/regression-models/linear-regression/main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import scipy.stats as ss
 import statsmodels.api as sm
 
-def linearRegression(x, y):
+def linearRegressionStatsModels(x, y):
   X = sm.add_constant(x)
   model = sm.OLS(y,X)
   results = model.fit()
@@ -21,7 +21,36 @@ def linearRegression(x, y):
   plt.text(23, 10, ec)
   plt.show()
 
-linearRegression(
-  [16, 32, 48, 56, 64, 80],
-  [10, 15, 20, 22, 30, 32]
-)
+def linearRegressionLeastSquare(x, y):
+  n = len(x)
+
+  # Finding required sum for least square methods
+  sumX,sumX2,sumY,sumXY = 0,0,0,0
+  for i in range(n):
+      sumX = sum(x)
+      sumX2 = sum([i ** 2 for i in x])
+      sumY = sum(y)
+      sumXY = sum([x * y for (x, y) in zip(x, y)])
+
+
+
+  # Obtaining a and b
+  b = (n * sumXY - sumX * sumY)/(n*sumX2 - sumX * sumX)
+  a = (sumY - b*sumX)/n
+
+  # Displaying coefficients a, b & equation
+  print("\nCoefficients are:")
+  print("a: ", a)
+  print("b: ", b)
+
+  print("\nEquation is:")
+  print(f'y = {a} + {b}x')
+
+  # Plot regression model
+  y_pred = []
+  for i in range(n):
+    y_pred.append(a+b*x[i])
+
+  plt.scatter(x, y)
+  plt.plot(x, y_pred, c='blue', linewidth=2)
+  plt.show()
