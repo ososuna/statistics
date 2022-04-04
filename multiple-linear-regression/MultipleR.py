e324cef8e513f8d4aea133002e4f4632f482b99b
ivai <ismb9@hotmail.com>
2022-04-04 15:24:38 -0500

    nuevo ejercicio

--- multiple-linear-regression/MultipleR.py
+++ multiple-linear-regression/MultipleR.py
@@ -1,57 +1,45 @@
-# %%
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from mpl_toolkits.mplot3d import Axes3D
 
-# %%
-weather_data_m = pd.read_csv('/home/ivanm/Desktop/repos/statistics/multiple-linear-regression/data/weatherHistory.csv', nrows=2000)
-weather_data_m
+estaturaMadre = [63,37,64,60,65,67,59,60,58,72,63,67,62,69,63,64,63,64,60,65]
+estaturaPadre = [64,65,67,72,72,72,67,71,66,75,69,70,69,62,66,76,69,68,66,68]
+estaturaHija = [58.6,64.7,65.3,61,65.4,67.4,60.9,63.1,60,71.1,62.2,67.2,63.4,68.4,62.2,64.7,59.6,61,64,65.4]
+vInd = [estaturaMadre,estaturaPadre]
 
-# %%
-weather_features = ['Temperature (C)','Wind Speed (km/h)', 'Pressure (millibars)']
 
-X = weather_data_m[weather_features]
-y = weather_data_m.Humidity
+X = vInd
+y = estaturaHija
 
 plt.subplot(2,2,1)
-plt.scatter(X['Temperature (C)'], y)
+plt.scatter(X[0], y)
 plt.subplot(2,2,2)
-plt.scatter(X['Wind Speed (km/h)'], y)
-plt.subplot(2,2,3)
-plt.scatter(X['Pressure (millibars)'], y)
+plt.scatter(X[1], y)
 
-# %%
-X = X.drop('Pressure (millibars)', 1)
-X
-
-# %%
-from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-x1 = X['Temperature (C)']
-x2 = X['Wind Speed (km/h)']
+x1 = X[0]
+x2 = X[1]
 
 ax.scatter(x1, x2, y, c='r', marker='o')
 
-ax.set_xlabel('Temperature (C)')
-ax.set_ylabel('Wind Speed (km/h)')
-ax.set_zlabel('Humidity')
+ax.set_xlabel('Estatura Madre')
+ax.set_ylabel('Estatura Padre')
+ax.set_zlabel('Estatura Hija')
 
-# %%
-from sklearn.linear_model import LinearRegression
+
 
 mlr_model = LinearRegression()
 mlr_model.fit(X, y)
 
-# %%
 theta0 = mlr_model.intercept_
 theta1, theta2 = mlr_model.coef_
 theta0, theta1, theta2
 
-# %%
 y_pred = mlr_model.predict([[15,21]])
 y_pred
-
-
